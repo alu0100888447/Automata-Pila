980a813380eac7e66b1ea5252b4e8005189dae76
Imar Abreu Diaz <alu0100888447@ull.edu.es>
2017-10-03 19:08:12 +0100

    Iniciamos el proyecto.

---
+++ cmake-build-debug/CMakeFiles/AP.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/AP.dir/main.cpp.o"
+  "AP.pdb"
+  "AP"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/AP.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for AP.
+# This may be replaced when dependencies are built.
