1f5165fc50c0198544f04251338dd43e6cdc56ed
Imar Abreu Diaz <alu0100888447@ull.edu.es>
2017-10-03 19:42:37 +0100

    Actualizamos la clase Nodo

--- include/Nodo.h
+++ include/Nodo.h
@@ -13,7 +13,16 @@
 using namespace std;
 
 class Nodo {
-
+private:
+    string id_;
+    SetTransiciones transiciones_;
+public:
+    Nodo();
+    virtual ~Nodo();
+    const string &getId_() const;
+    const SetTransiciones &getTransiciones_() const;
+    void setId_(const string &id_);
+    void setTransiciones_(const SetTransiciones &transiciones_);
 };
 
 
--- src/Nodo.cpp
+++ src/Nodo.cpp
@@ -3,3 +3,27 @@
 //
 
 #include "../include/Nodo.h"
+
+Nodo::Nodo() {
+
+}
+
+Nodo::~Nodo() {
+
+}
+
+const string Nodo::getId_() const {
+    return id_;
+}
+
+const SetTransiciones Nodo::getTransiciones_() const {
+    return transiciones_;
+}
+
+void Nodo::setId_(const string &id_) {
+    Nodo::id_ = id_;
+}
+
+void Nodo::setTransiciones_(const SetTransiciones &transiciones_) {
+    Nodo::transiciones_ = transiciones_;
+}
