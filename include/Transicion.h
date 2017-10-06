8787e209f29fabf98180d65155bf71adab2d086f
Imar Abreu Diaz <alu0100888447@ull.edu.es>
2017-10-06 10:12:47 +0100

    Actualizamos la clase Transicion

--- include/Transicion.h
+++ include/Transicion.h
@@ -6,11 +6,24 @@
 #define AP_TRANSICION_H
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 class Transicion {
+private:
+    string estadoSiguiente_;
+    char entrada_;
+    char cabezaPila;
+    string meterPila;
+public:
+    Transicion();
+    Transicion(string estadoSiguiente, char entrada, char cabezaPila, string meterPila);
+    Transicion(const Transicion &cp);
+    virtual ~Transicion();
+    void operator=(const Transicion &cp);
 
+    friend operator<<(ostream &out, const Transicion &cp);
 };
 
 
--- src/Transicion.cpp
+++ src/Transicion.cpp
@@ -3,3 +3,26 @@
 //
 
 #include "../include/Transicion.h"
+
+Transicion::Transicion() {}
+
+Transicion::Transicion(string estadoSiguiente, char entrada, char cabezaPila, string meterPila) {
+
+}
+
+Transicion::Transicion(const Transicion &cp) {
+
+}
+
+Transicion::~Transicion() {
+
+}
+
+void Transicion::operator=(const Transicion &cp) {
+
+}
+
+long operator<<(ostream &out, const Transicion &cp) {
+    return 0;
+}
+
