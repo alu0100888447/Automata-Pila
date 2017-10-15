649db87310bf4977e8630c5458ed5a6e0a36b8b2
Imar Abreu Diaz <alu0100888447@ull.edu.es>
2017-10-15 13:54:53 +0100

    Comentarios clase Transicion

--- include/Transicion.h
+++ include/Transicion.h
@@ -17,37 +17,125 @@
 
 using namespace std;
 
+/**
+ * @class Transicion
+ * @brief Representa una transicion de un estado de un automata de pila.
+ */
 class Transicion {
 private:
     int idNumber_;
     string estadoSiguiente_;
     string entrada_;
     string cabezaPila_;
-    vector <string> meterPila_;
+    vector<string> meterPila_;
 public:
+    /**
+     * @brief Constructor por defecto.
+     */
     Transicion();
-    Transicion(int idNumber, string estadoSiguiente, string entrada, string cabezaPila, vector <string> meterPila);
+    /**
+     * @brief Constructor parametrizado.
+     * @param idNumber
+     * @param estadoSiguiente
+     * @param entrada
+     * @param cabezaPila
+     * @param meterPila
+     */
+    Transicion(int idNumber, string estadoSiguiente, string entrada, string cabezaPila, vector<string> meterPila);
+    /**
+     * @brief Constructor de copia.
+     * @param cp
+     */
     Transicion(const Transicion &cp);
+    /**
+     * @brief Destructor por defecto.
+     */
     virtual ~Transicion();
+    /**
+     * @brief Funcion que limpia el objeto.
+     */
     void clearTransicion();
-
+    /**
+     * @brief Getter idNumber_.
+     * @return
+     */
     int getIdNumber_() const;
+    /**
+     * @brief Setter idNumber_.
+     * @param idNumber_
+     */
     void setIdNumber_(int idNumber_);
+    /**
+     * @brief Getter estadoSiguiente_.
+     * @return
+     */
     const string &getEstadoSiguiente_() const;
+    /**
+     * @brief Setter estadoSiguiente_.
+     * @param estadoSiguiente_
+     */
     void setEstadoSiguiente_(const string &estadoSiguiente_);
+    /**
+     * @brief Getter entrada_.
+     * @return
+     */
     const string &getEntrada_() const;
+    /**
+     * @brief Setter entrada_.
+     * @param entrada_
+     */
     void setEntrada_(const string &entrada_);
+    /**
+     * @brief Getter cabezaPila_.
+     * @return
+     */
     const string &getCabezaPila_() const;
+    /**
+     * @brief Setter cabezaPila_.
+     * @param cabezaPila_
+     */
     void setCabezaPila_(const string &cabezaPila_);
+    /**
+     * @brief Getter meterPila_.
+     * @return
+     */
     const vector<string> &getMeterPila_() const;
+    /**
+     * @brief Setter meterPila_.
+     * @param meterPila_
+     */
     void setMeterPila_(const vector<string> &meterPila_);
-
-    Transicion& operator=(const Transicion &cp);
+    /**
+     * @brief Sobrecarga operador =.
+     * @param cp
+     * @return
+     */
+    Transicion &operator=(const Transicion &cp);
+    /**
+     * @brief Sobrecarga operador ==.
+     * @param cp
+     * @return
+     */
     bool operator==(const Transicion &cp) const;
+    /**
+     * @brief Sobrecarga operador !=.
+     * @param cp
+     * @return
+     */
     bool operator!=(const Transicion &cp) const;
+    /**
+     * @brief Sobrecarga operador <.
+     * @param cp
+     * @return
+     */
     bool operator<(const Transicion &cp) const;
-
-    friend ostream& operator<<(ostream &out, const Transicion &cp);
+    /**
+     * @brief Sobrecarga operador <<.
+     * @param out
+     * @param cp
+     * @return
+     */
+    friend ostream &operator<<(ostream &out, const Transicion &cp);
 };
 
 
